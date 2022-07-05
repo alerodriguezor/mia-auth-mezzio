76a4c380a2156ea7efb47882a9eef3cb2c65394f
Matias Camiletti <matias.camiletti@gmail.com>
2022-07-05 10:56:16 -0300

    Verify old password.

--- src/Handler/ChangePasswordHandler.php
+++ src/Handler/ChangePasswordHandler.php
@@ -2,6 +2,8 @@
 
 namespace Mia\Auth\Handler;
 
+use Mia\Core\Helper\MiaErrorHelper;
+
 /**
  * Description of ChangePasswordHandler
  *
@@ -13,9 +15,14 @@ class ChangePasswordHandler extends \Mia\Auth\Request\MiaAuthRequestHandler
     public function handle(\Psr\Http\Message\ServerRequestInterface $request): \Psr\Http\Message\ResponseInterface
     {
         // Obtener parametros
+        $old = $this->getParam($request, 'old_password', '');
         $password = $this->getParam($request, 'password', '');
         // Obtener usuario
         $item = $this->getUser($request);
+        // Verify if password is correct
+        if($item->password != 'password_not_assigned' && !\Mia\Auth\Model\MIAUser::verifyPassword($old, $item->password)){
+            return MiaErrorHelper::toLangEs($request, -3, 'La contraseña es incorrecta', 'Password is not correct');
+        }
         // Cambiar valores
         $item->password = \Mia\Auth\Model\MIAUser::encryptPassword($password);
         // Guardar nueva contraseña
